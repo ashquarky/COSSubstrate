49b9964b37afa6d77a88fc198442109420396411
Ash <quarktheawesome@gmail.com>
2016-12-13 12:10:36 +1100

    Initial commit

---
+++ src/entry.c
@@ -0,0 +1,8 @@
+/*	Cafe OS Substrate
+	This is the Installer.
+	Licensed under MIT, visit https://github.com/QuarkTheAwesome/COSSubstrate for more details.
+*/
+#include "main.h"
+int __entry_menu(int argc, char **argv) {
+	return Menu_Main();
+}
---
+++ src/main.c
@@ -0,0 +1,7 @@
+/*	Cafe OS Substrate
+	This is the Installer.
+	Licensed under MIT, visit https://github.com/QuarkTheAwesome/COSSubstrate for more details.
+*/
+int Menu_Main() {
+	return 0;
+}
