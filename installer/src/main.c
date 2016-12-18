5401dda7936493fc5075e312b957c8f9a03a9a1a
Ash <quarktheawesome@gmail.com>
2016-12-19 10:14:14 +1100

    Clean out testing stuff, time to do the things!

--- installer/src/main.c
+++ installer/src/main.c
@@ -28,43 +28,12 @@ int Menu_Main() {
 	log_init("192.168.192.36");
 	log_print("Hello World!\n");
 
-	InstallKernFunctions();
+	//Do code things here
 
-	unsigned int* nom = malloc(0x10);
-	log_printf("Memory allocated at 0x%08X\n", nom);
-	nom[0] = 0x00000000;
-	log_printf("Pre-write memory value: 0x%08X\n", *nom);
-	log_print("Compare this value with the kern_write test below.\n");
-	kern_write(nom, 0x69696969);
-	log_printf("kern_write test (expect 0x69696969): 0x%08X\n", *nom);
-	unsigned int nom2 = kern_read(nom);
-	log_printf("kern_read test (expect 0x69696969): 0x%08X\n", nom2);
-
-	RunCodeAsKernel(&SetupBATs, 0);
-	log_print("BAT mappings set up!\n");
-
-	unsigned int* bat = (unsigned int*)0x60000010;
-	InstallAltExceptionHandler();
-	unsigned int res = *bat;
-	InstallExceptionHandler();
-	log_printf("Value of 0x%08X (0xDEADC0DE = fail): 0x%08X\n", bat, res);
-
-	RunCodeAsKernel(&ClearBATs, 0);
-
-	FILE* fd = fopen("sd:/lib.elf", "rb");
-	fseek(fd, 0L, SEEK_END);
-	unsigned int fs = ftell(fd);
-	fseek(fd, 0L, SEEK_SET);
-
-	char buf[4];
-	fread((void*)buf, 4, 1, fd);
-	log_printf("fd: %08X, size: %08X, f[0][1][2]: %c%c%c\n", fd, fs, buf[0], buf[1], buf[2]);
-
-	fclose(fd);
-	free(nom);
-	unmount_sd_fat("sd");
 	log_print("Quitting...\n");
 	log_print("------------------------------------\n\n\n");
+
+	unmount_sd_fat("sd");
 	log_deinit();
 
 	return 0;
