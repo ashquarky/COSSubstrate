5904232afea2fc51184f72eae9d3e8d78191205c
Ash <quarktheawesome@gmail.com>
2016-12-22 18:34:57 +1100

    Fix licensing and add headers across codebase

--- installer/src/dynamic_libs/mem_functions.h
+++ installer/src/dynamic_libs/mem_functions.h
@@ -1,3 +1,29 @@
+/*	Cafe OS Substrate Installer
+
+	mem_functions.h - Exposes memory functions from dynamic_libs.
+	No parter file.
+
+	https://github.com/QuarkTheAwesome/COSSubstrate
+
+	Copyright (c) 2016 Ash (QuarkTheAwesome)
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to
+	deal in the Software without restriction, including without limitation the
+	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+
 extern void *(* MEMAllocFromExpHeapEx)(int heap, unsigned int size, int align);
 extern int (* MEMCreateExpHeapEx)(void* address, unsigned int size, unsigned short flags);
 extern void *(* MEMDestroyExpHeap)(int heap);
--- installer/src/main.h
+++ installer/src/main.h
@@ -1,19 +1,33 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+/*	Cafe OS Substrate Installer
 
-#include "common/types.h"
-#include "dynamic_libs/os_functions.h"
+	main.h - Exposes main application logic to entry.c.
+	Paired with main.c.
 
-/* Main */
-#ifdef __cplusplus
-extern "C" {
-#endif
+	https://github.com/QuarkTheAwesome/COSSubstrate
 
-//! C wrapper for our C++ functions
-int Menu_Main(void);
+	Copyright (c) 2016 Ash (QuarkTheAwesome)
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to
+	deal in the Software without restriction, including without limitation the
+	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
 
-#ifdef __cplusplus
-}
-#endif
+#ifndef MAIN_H
+#define MAIN_H
+
+/*	Main function.
+*/
+int Menu_Main();
 
 #endif
